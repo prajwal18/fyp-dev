80c00bb853019cdce5d2e9a9ff698e2a1fda434e
Prajwal Gautam <prajwalgautam08@gmail.com>
2023-01-28 01:20:17 +0545

    Made Ui changes to Dashboard and SignUpDonePage

--- User Portal/user-portal-ui/components/LoginSignup/SignUpDonePage.tsx
+++ User Portal/user-portal-ui/components/LoginSignup/SignUpDonePage.tsx
@@ -1,9 +1,7 @@
 
 import { useRouter } from "next/router";
-import styled from "styled-components";
 // MUI Components
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 // MUI Components
@@ -18,25 +16,10 @@ import SuccessLottie from "@/public/Lottie/98741-success.json";
 import LoginSignupLayout from "../Layout/LoginSignupLayout";
 
 
-//Styled Components
-const PageContainer = styled(Box)`
-    width: 100%;
-    max-width: 700px;
-    background: white;
-    padding: 50px 20px;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-`;
-//Styled Components
-
 const SignUpDoneContainer = () => {
     const router = useRouter();
     return (
-        <PageContainer>
+        <Stack sx={{width: "100%", maxWidth: "700px", background: "white", padding: "50px 20px", borderRadius: "5px", textAlign: "center"}}>
             <Lottie animationData={SuccessLottie} loop={true} style={{height: "300px"}}/>
             <Stack spacing={1} sx={{ marginTop: "30px" }}>
                 <Typography variant="h4" component="h4">Sign Up Done</Typography>
@@ -52,7 +35,7 @@ const SignUpDoneContainer = () => {
                 <ArrowBackIosIcon />
                 <Typography variant="h6">Go To Login</Typography>
             </Button>
-        </PageContainer>
+        </Stack>
     )
 }
 
--- User Portal/user-portal-ui/pages/Dashboard.tsx
+++ User Portal/user-portal-ui/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
+
 const Dashboard = () => {
     return(
-        <>
-        </>
+        <></>
     );
 }
 export default Dashboard;
